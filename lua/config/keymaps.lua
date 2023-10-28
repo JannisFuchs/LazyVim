679659e77db8e5645ff9c2ba2cdd8af3c43cbf59
huchsle <janifu02@gmail.com>
2023-10-28 19:22:51 +0200

    transparent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+keymap("n", "<C-k>", "i[latex]<cr>[/latex]<ESC>", opts)
+keymap("n", "<leader>ft", "<cmd>ToggleTerm<cr>", opts)
